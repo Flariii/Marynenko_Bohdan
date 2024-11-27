5f6ef0b8a5f14d12e537f2b8800ce05515251b8f
Flariii <homa4467@gmail.com>
2024-11-27 08:57:37 +0200

    ad ne w file

--- EXEL _4/EXEL _4/obj/Release/net8.0/EXEL _4.AssemblyInfo.cs
+++ EXEL _4/EXEL _4/obj/Release/net8.0/EXEL _4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EXEL _4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f3be7f9b37923a65d0e8d3c16dbaafcd5a4352a")]
 [assembly: System.Reflection.AssemblyProductAttribute("EXEL _4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EXEL _4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
